9d79786f4e260b6649a6a7cddc189f5dd849aabb
shitikyan <shitikyan@gmail.com>
2023-12-21 18:37:22 +0400

    Edit Profile UI

---
+++ Threads-App-Swift-UI/Core/Profile/EditProfile/View/EditProfileView.swift
@@ -0,0 +1,95 @@
+//
+//  EditProfileView.swift
+//  Threads-App-Swift-UI
+//
+//  Created by Solicy Ios on 21.12.23.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    
+    @State private var bio = ""
+    @State private var link = ""
+    @State private var isPrivateProfile = false
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(.systemGroupedBackground)
+                    .ignoresSafeArea(edges: [.bottom, .horizontal])
+                VStack {
+                    // Name and profile image
+                    VStack {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text("Name")
+                                    .fontWeight(.semibold)
+                                Text("Charles Leclerc")
+                            }
+                            Spacer()
+                            
+                            CircularProfileImageView()
+                        }
+                        
+                        Divider()
+                        // Bio field
+                        
+                        VStack(alignment: .leading) {
+                            Text("Bio")
+                                .fontWeight(.semibold)
+                            TextField("Enter your bio...", text: $bio)
+                        }
+                        
+                        Divider()
+                        
+                        VStack(alignment: .leading) {
+                            Text("Link")
+                                .fontWeight(.semibold)
+                            TextField("Add link...", text: $link)
+                        }
+                        
+                        Divider()
+                        
+                        Toggle("Private Profile", isOn: $isPrivateProfile)
+                    }
+                }
+                .font(.footnote)
+                .padding()
+                .background(.white)
+                .cornerRadius(10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color(.systemGray4), lineWidth: 1)
+                }
+                .padding()
+            }
+            .navigationTitle("Edit Profile")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
