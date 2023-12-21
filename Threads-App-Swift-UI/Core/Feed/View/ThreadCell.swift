bc3246f95eb219a7d889686ffb571852ee2d48bc
shitikyan <shitikyan@gmail.com>
2023-12-21 17:50:45 +0400

    Feed View UI

---
+++ Threads-App-Swift-UI/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,82 @@
+//
+//  ThreadCell.swift
+//  Threads-App-Swift-UI
+//
+//  Created by Solicy Ios on 21.12.23.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                
+                Image("alex-profile-image")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        
+                        Text("alex.2001")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray3))
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(Color(.darkGray))
+                        }
+                    }
+                    
+                    Text("Formula 1 champion")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundStyle(.black)
+                    .padding(.vertical, 8)
+                }
+            }
+            .padding()
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
