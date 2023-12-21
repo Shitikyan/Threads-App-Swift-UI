c513dadb18fbb623fd724ecf2e3d5db5349e3008
shitikyan <shitikyan@gmail.com>
2023-12-21 15:31:28 +0400

    Created Empty Project

---
+++ Threads-App-Swift-UI/Threads_App_Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Threads_App_Swift_UIApp.swift
+//  Threads-App-Swift-UI
+//
+//  Created by Solicy Ios on 21.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Threads_App_Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
